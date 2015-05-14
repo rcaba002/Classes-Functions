c79095d9d7c19f57dbd05ae69ddc0ea8929b21ce
Rob C <robcabardo@gmail.com>
2015-05-14 14:13:04 -0500

    Initial commit

---
+++ CarShopping/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopping
+{
+    // Bug: When you remove a vehicle it removes all vehicles with same specification
+    // Bug: When you do not enter a number for vehicle year, program breaks
+    // Bug: When looking up or removing vehicle, when you specify how to look up the vehicle
+    //      and enter a non-matching string, there is no notification, returns to menu
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> inventory = new List<Car>();
+
+            Menu(ref inventory);
+        }
+
+        public static void Menu(ref List<Car> inventory)
+        {
+            Console.WriteLine();
+            Console.WriteLine("A - Add vehicle      L - Lookup vehicle(s)");
+            Console.WriteLine("R - Remove vehicle   V - View entire inventory");
+            Console.WriteLine("E - Exit");
+            Console.WriteLine();
+
+            Console.Write("What would you like to do? ");
+            string userInput = Console.ReadLine().ToLower();
+
+            if (userInput == "a")
+            {
+                Vehicles.addCars(ref inventory);
+                Menu(ref inventory);
+            }
+            else if (userInput == "r")
+            {
+                Vehicles.removeCars(ref inventory);
+                Menu(ref inventory);
+            }
+            else if (userInput == "l")
+            {
+                Vehicles.lookupCars(ref inventory);
+                Menu(ref inventory);
+            }
+            else if (userInput == "e")
+                Environment.Exit(0);
+            else if (userInput == "v")
+            {
+                Vehicles.entireInventory(ref inventory);
+                Menu(ref inventory);
+            }
+            else
+            {
+                Console.WriteLine("I did not understand that.");
+                Menu(ref inventory);
+            }
+        }
+    }
+
+    class Vehicles
+    {
+        internal static void addCars(ref List<Car> inventory)
+        {
+            Console.WriteLine();
+            Console.Write("What is the make? ");
+            string make = Console.ReadLine();
+            Console.Write("What is the model? ");
+            string model = Console.ReadLine();
+            Console.Write("What is the year? ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is the color? ");
+            string color = Console.ReadLine();
+            Car car = new Car(make, model, year, color);
+            inventory.Add(car);
+            Console.WriteLine();
+            Console.WriteLine("{0} - {1} - {2} - {3} - Added", make, model, year, color);
+        }
+
+        internal static void removeCars(ref List<Car> inventory)
+        {
+            Console.WriteLine();
+            Console.WriteLine("M - Make     O - Model");
+            Console.WriteLine("Y - Year     C - Color     E - Go Back");
+            Console.WriteLine();
+            Console.Write("How would you like to look up the car? ");
+            string userInput = Console.ReadLine().ToUpper();
+
+            if (userInput == "M")
+            {
+                Console.Write("What is the make? ");
+                string make = Console.ReadLine();
+                Console.WriteLine();
+
+                foreach (Car vehicle in inventory)
+                {
+                    if (vehicle.Make == make)
+                    {
+                        Console.WriteLine("{0} - {1} - {2} - {3} - Removed", vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color);
+                        inventory.RemoveAll(a => vehicle.Make == make);
+                        break;
+                    }
+                }
+            }
+
+            else if (userInput == "O")
+            {
+                Console.Write("What is the model? ");
+                string model = Console.ReadLine();
+                Console.WriteLine();
+
+                foreach (Car vehicle in inventory)
+                {
+                    if (vehicle.Model == model)
+                    {
+                        Console.WriteLine("{0} - {1} - {2} - {3} - Removed", vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color);
+                        inventory.RemoveAll(a => vehicle.Model == model);
+                        break;
+                    }
+                }
+            }
+
+            else if (userInput == "Y")
+            {
+                Console.Write("What is the year? ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                foreach (Car vehicle in inventory)
+                {
+                    if (vehicle.Year == year)
+                    {
+                        Console.WriteLine("{0} - {1} - {2} - {3} - Removed", vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color);
+                        inventory.RemoveAll(a => vehicle.Year == year);
+                        break;
+                    }
+                }
+            }
+
+            else if (userInput == "C")
+            {
+                Console.Write("What is the color? ");
+                string color = Console.ReadLine();
+                Console.WriteLine();
+
+                foreach (Car vehicle in inventory)
+                {
+                    if (vehicle.Color == color)
+                    {
+                        Console.WriteLine("{0} - {1} - {2} - {3} - Removed", vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color);
+                        inventory.RemoveAll(a => vehicle.Color == color);
+                        break;
+                    }
+                }
+            }
+
+            else if (userInput == "E")
+                Program.Menu(ref inventory);
+
+            else
+            {
+                Console.WriteLine("I did not understand that.");
+                Vehicles.lookupCars(ref inventory);
+            }
+        }
+
+        internal static void lookupCars(ref List<Car> inventory)
+        {
+            Console.WriteLine();
+            Console.WriteLine("M - Make     O - Model");
+            Console.WriteLine("Y - Year     C - Color     E - Go Back");
+            Console.WriteLine();
+            Console.Write("How would you like to look up the car? ");
+            string userInput = Console.ReadLine().ToUpper();
+
+            if (userInput == "M")
+            {
+                Console.Write("What is the make? ");
+                string make = Console.ReadLine();
+                Console.WriteLine();
+
+                foreach (Car vehicle in inventory)
+                {
+                    if (vehicle.Make == make)
+                        Console.WriteLine("{0} - {1} - {2} - {3}", vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color);
+                }
+            }
+
+            else if (userInput == "O")
+            {
+                Console.Write("What is the model? ");
+                string model = Console.ReadLine();
+                Console.WriteLine();
+
+                foreach (Car vehicle in inventory)
+                {
+                    if (vehicle.Model == model)
+                        Console.WriteLine("{0} - {1} - {2} - {3}", vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color);
+                }
+            }
+
+            else if (userInput == "Y")
+            {
+                Console.Write("What is the year? ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                foreach (Car vehicle in inventory)
+                {
+                    if (vehicle.Year == year)
+                        Console.WriteLine("{0} - {1} - {2} - {3}", vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color);
+                }
+            }
+
+            else if (userInput == "C")
+            {
+                Console.Write("What is the color? ");
+                string color = Console.ReadLine();
+                Console.WriteLine();
+
+                foreach (Car vehicle in inventory)
+                {
+                    if (vehicle.Color == color)
+                    {
+                        Console.WriteLine("{0} - {1} - {2} - {3}", vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color);
+                    }
+                }
+            }
+
+            else if (userInput == "E")
+                Program.Menu(ref inventory);
+
+            else
+            {
+                Console.WriteLine("I did not understand that.");
+                Vehicles.lookupCars(ref inventory);
+            }
+        }
+
+        internal static void entireInventory(ref List<Car> inventory)
+        {
+            Console.WriteLine();
+            foreach (Car vehicle in inventory)
+            {
+                Console.WriteLine("{0} - {1} - {2} - {3}", vehicle.Make, vehicle.Model, vehicle.Year, vehicle.Color);
+            }
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+    }
+}
---
+++ RecipeBuilder/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBuilder
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Create my ingredient
+            Ingredient BrownSugar = new Ingredient();
+            BrownSugar.Amount = 1.0;
+            BrownSugar.Calories = 200;
+            BrownSugar.Carbohydrates = 500;
+            BrownSugar.Protein = 13;
+            BrownSugar.Sodium = 500;
+            BrownSugar.Type = "Brown Sugar";
+
+            Ingredient Butter = new Ingredient();
+            Butter.Amount = 2.0;
+            Butter.Calories = 1000;
+            Butter.Carbohydrates = 300;
+            Butter.Protein = 26;
+            Butter.Sodium = 10000;
+            Butter.Type = "Lb of Butter";
+
+            Ingredient ChocolateChips = new Ingredient();
+            ChocolateChips.Amount = 3;
+            ChocolateChips.Calories = 200;
+            ChocolateChips.Carbohydrates = 600;
+            ChocolateChips.Protein = 40;
+            ChocolateChips.Sodium = 100000;
+            ChocolateChips.Type = "Chocolate Chips";
+
+            //Create a key-value storage device (in this case, a Dictionary)
+            Dictionary<int, Ingredient> ingredients = new Dictionary<int, Ingredient>();
+
+            //Add the ingredient, along with a key, to the Dictionary
+            ingredients.Add(1, BrownSugar);
+            ingredients.Add(2, Butter);
+            ingredients.Add(3, ChocolateChips);
+
+            //Create our recipe object and populate its properties
+            Recipe ChocolateChipCookies = new Recipe();
+            ChocolateChipCookies.Type = "Chocolate Chip Cookies!";
+            ChocolateChipCookies.Ingredients = ingredients;
+            ChocolateChipCookies.Rating = 8;
+            Console.WriteLine(ChocolateChipCookies.ToString());
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Recipe
+    {
+        public Dictionary<int, Ingredient> Ingredients { get; set; }
+        public int Rating { get; set; }
+        public string Type { get; set; }
+
+        //Total the Nutritional info
+        public string GetNutritionalInfo()
+        {
+            double caloriesTotal = 0, carbsTotal = 0, proteinTotal = 0;
+
+            foreach (KeyValuePair<int, Ingredient> item in Ingredients)
+            {
+                caloriesTotal += item.Value.Calories;
+                carbsTotal += item.Value.Carbohydrates;
+                proteinTotal += item.Value.Protein;
+            }
+
+            string result = "\n\nCalories: " + caloriesTotal + "\nCarbs: " + carbsTotal + "\nProtein: " + proteinTotal;
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            string returnVal = "Recipe for: " + Type + "\nRating: " + Rating + "\nIngredients:";
+
+            foreach (KeyValuePair<int, Ingredient> item in Ingredients)
+            {
+                returnVal += "\n" + item.Value.Type + " - Amount: " + item.Value.Amount;
+            }
+
+            returnVal += GetNutritionalInfo();
+
+            return returnVal;
+        }
+    }
+
+    public class Ingredient
+    {
+        public double Amount { get; set; }
+        public double Calories { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Protein { get; set; }
+        public double Sodium { get; set; }
+        public double Sugar { get; set; }
+        public string Type { get; set; }
+    }
+}
+
+//1.Design and write a series of appropriate classes to model a recipe. The recipe should 
+//include all ingredients needed for making the recipe, all amounts required, and basic nutritional 
+//information such as calories, protein, fat, sugar, sodium. You do not have to make the nutritional 
+//information accurate, you may enter your own values here. Allow the user to assign a rating to the recipe.
+//2.Write a program that allows you to either create a new recipe or view existing recipes. 
+//Make sure to encapsulate the appropriate functionality into functions for easy reuse, and to utilize 
+//proper data types. Obviously, to view multiple recipes, you will need to store them in an array.
+//3.(Bonus) Include functionality that allows the user to choose a recipe based on highest rating.
